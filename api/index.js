496c29be1eca5b49d9a16eaec68f1c55ad163795
Zach <zachmvm@gmail.com>
2025-03-12 20:12:47 -0400

    update api index

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import serverless from "serverless-http";
 import cors from "cors";
-import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
 const app = express();
 const port = 3001;
 
