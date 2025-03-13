2c115ddb7b212750a77f58a42ba147b225f3c02e
Zach Whitsett <111324096+zachmvm@users.noreply.github.com>
2025-03-12 20:51:21 -0400

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -20,7 +20,9 @@ export default function App() {
     try {
       const { data } = await axios.get(API_URL);
       setTasks(data);
-    } catch (err) {}
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
